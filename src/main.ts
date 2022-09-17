cfff72f508c30957930895a75f0a5e94029f946a
Vinicius Rech <contato@vinicius-rech.dev>
2022-09-17 15:39:20 -0300

    chore: enabled cors.

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import { AppModule } from './api/app/app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   const config = new DocumentBuilder()
       .setTitle('API')
       .setDescription('Documentação')
       .setVersion('1.0.0')
       .build();
   const  document = SwaggerModule.createDocument(app, config);
-  app.enableCors()
   SwaggerModule.setup('api', app, document)
   await app.listen(process.env.PORT || 3000);
 }
