802b9c02de920ec7588bbef8920a33a9a5eece7f
Vinicius Rech <contato@vinicius-rech.dev>
2022-09-20 18:29:19 -0300

    refactor: dump code removed

--- src/api/task/task.controller.ts
+++ src/api/task/task.controller.ts
@@ -4,7 +4,6 @@ import {
   Param,
   Post,
   Body,
-  Put,
   Delete
 } from "@nestjs/common";
 import { Task as TaskModel } from "@prisma/client";
@@ -17,25 +16,19 @@ export class TaskController {
   constructor(private readonly taskService: TaskService) {
   }
 
-  @ApiTags('task')
-  @Post('task/new')
+  @ApiTags("task")
+  @Post("task/new")
   async createTask(
-      @Body() taskData: TaskModel,
+    @Body() taskData: TaskModel
   ): Promise<TaskModel> {
-      const {description} = taskData;
-      return this.taskService.createTask({
-          description
-      });
+    const { description } = taskData;
+    return this.taskService.createTask({
+      description
+    });
   }
 
-  // @ApiTags('blog-post')
-  // @Get('blog-post/:id')
-  // async getPostById(@Param('id') id: string): Promise<PostModel> {
-  //     return this.taskService.blogPost({id: Number(id)});
-  // }
-
-  @ApiTags('task')
-  @Get('task/all')
+  @ApiTags("task")
+  @Get("task/all")
   async get(): Promise<TaskModel[]> {
     return this.taskService.listAll({
       where: {
@@ -49,53 +42,19 @@ export class TaskController {
     });
   }
 
-  // @ApiTags('blog-posts')
-  // @Get('blog-posts/filtered-posts/:searchString')
-  // async getFilteredPosts(
-  //     @Param('searchString') searchString: string,
-  // ): Promise<PostModel[]> {
-  //     return this.taskService.blogPosts({
-  //         where: {
-  //             OR: [
-  //                 {
-  //                     title: {contains: searchString},
-  //                 },
-  //                 {
-  //                     content: {contains: searchString},
-  //                 },
-  //             ],
-  //         },
-  //     });
-  // }
-
-  // @ApiTags('blog-post')
-  // @Put('blog-post/publish/:id')
-  // async publishPost(@Param('id') id: string): Promise<PostModel> {
-  //     return this.taskService.updateBlogPost({
-  //         where: {id: Number(id)},
-  //         data: {published: true},
-  //     });
-  // }
-
-  @ApiTags('task')
-  @Delete('task/delete/:id')
-  async deleteTask(@Param('id') id: string): Promise<TaskModel> {
-      return this.taskService.deleteTask({id: Number(id)});
+  @ApiTags("task")
+  @Delete("task/delete/:id")
+  async deleteTask(@Param("id") id: string): Promise<TaskModel> {
+    return this.taskService.deleteTask({ id: Number(id) });
   }
-  // @ApiTags('task')
-  // @Post('task/mass-delete')
-  // async massDelete(@Body() ): Promise<TaskModel> {
-  //     return this.taskService.deleteTask({id: Number(id)});
-  // }
 
-  @ApiTags('task')
-  @Post('task/mass-delete')
+  @ApiTags("task")
+  @Post("task/mass-delete")
   async massDelete(
-    @Body() taskData: [],
+    @Body() taskData: []
   ) {
     return taskData.forEach((task: TaskProps) => {
-      return this.taskService.deleteTask({id: task.id})
-    })
+      return this.taskService.deleteTask({ id: task.id });
+    });
   }
-
 }
